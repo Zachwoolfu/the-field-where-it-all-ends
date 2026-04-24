eb4f8925cc32733ad1df41434971431af58a3322
Zed Pug <102879485+Zachwoolfu@users.noreply.github.com>
2026-04-24 11:20:19 -0700

    Refactor fight initiation logic and damage handling

--- TheFieldWhereItAllEnds/Systems/Functions.py
+++ TheFieldWhereItAllEnds/Systems/Functions.py
@@ -50,21 +50,29 @@ def Choose_Enemy(Enemy1,Enemy2,Enemy3):
 
 def Initiate_Fight(Local_Player,Ally1,Ally2,Ally3,Ally4,Enemy1,Enemy2,Enemy3):
     while True:
-        Plr_Dead = Local_Player.IsDied()
-        if Plr_Dead == True:
+        if Local_Player.IsDied() == True:
             return False 
+    
+        
+        
+        if Ally1 is not None:
+            Ally1.Status_Ending()
+            if Ally1.IsDied() == True:
+                Local_Player.Damage_Player(1)
+        if Ally2 is not None: 
+            Ally2.Status_Ending()
+            if Ally2.IsDied() == True:
+                Local_Player.Damage_Player(1)
+        if Ally3 is not None: 
+            Ally3.Status_Ending()
+            if Ally3.IsDied() == True:
+                Local_Player.Damage_Player(1)
+        if Ally4 is not None: 
+            Ally4.Status_Ending()
+            if Ally4.IsDied() == True:
+                Local_Player.Damage_Player(1)
 
         Selected_Unit = Local_Player.SelectUnit(Ally1,Ally2,Ally3,Ally4)
-        
-        if Ally1 is not None and Ally1.IsDied() == True:
-            Local_Player.Take_Damage(1)
-        if Ally2 is not None and Ally2.IsDied() == True:
-            Local_Player.Take_Damage(1)
-        if Ally3 is not None and Ally3.IsDied() == True:
-            Local_Player.Take_Damage(1)
-        if Ally4 is not None and Ally4.IsDied() == True:
-            Local_Player.Take_Damage(1)
-
         if Selected_Unit == 1:
             Ally1.Action(Enemy1,Enemy2,Enemy3)
 
@@ -80,18 +88,27 @@ def Initiate_Fight(Local_Player,Ally1,Ally2,Ally3,Ally4,Enemy1,Enemy2,Enemy3):
             Ally4.Action(Enemy1,Enemy2,Enemy3)
             if Ally1.IsDied() == True:
                 Local_Player.Take_Damage(1)
+        count = 0
+
 
         Timed_Text("Enemies turn!",0.03,True,True)
         if Enemy1 is not None:
             Enemy1.Action(Ally1,Ally2,Ally3,Ally4)
+            count += 1
             if Enemy1.IsDied() == True:
                 Enemy1 = None
+                count -= 1
         if Enemy2 is not None:
             Enemy2.Action(Ally1,Ally2,Ally3,Ally4)
+            count += 1
             if Enemy2.IsDied() == True:
                 Enemy2 = None
+                count -= 1
         if Enemy3 is not None:
             Enemy3.Action(Ally1,Ally2,Ally3,Ally4)
+            count += 1
             if Enemy3.IsDied() == True:
                 Enemy3 = None
-        
+                count -= 1
+        if count == 0:
+            return True
