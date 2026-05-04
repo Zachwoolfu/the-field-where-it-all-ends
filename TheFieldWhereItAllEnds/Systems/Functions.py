354d9ee75061f53afe5586d617e27abb8a416e98
Zed Pug <102879485+Zachwoolfu@users.noreply.github.com>
2026-05-04 11:19:49 -0700

    Refactor enemy selection and combat logic

--- TheFieldWhereItAllEnds/Systems/Functions.py
+++ TheFieldWhereItAllEnds/Systems/Functions.py
@@ -29,84 +29,134 @@ def Choose_Enemy(Enemy1,Enemy2,Enemy3):
             System_Clear()
             Enemies = [Enemy1, Enemy2, Enemy3]
             Count = 0
+            Chosen_Enemy = None
             for enemy in Enemies:
                 if enemy is not None:
                     Count += 1
                     print(f"{Count}: {enemy.Name} | HP: {enemy.Health}")
-
+    
             Choice = int(input("Input: "))
-            if Choice == 1:
-                if Enemy1 is not None:    
-                    return Enemy1
-            if Choice == 2:
-                if Enemy2 is not None:
-                    return Enemy2
-            if Choice == 3:
-                if Enemy3 is not None:
-                    return Enemy3
+            L_Count = 0
+            for enemy in Enemies:
+                if enemy is not None:
+                    L_Count += 1
+                    if Choice == L_Count:
+                        Chosen_Enemy = enemy
+                        return Chosen_Enemy
         except:
             Timed_Text('Please choose a number!!!',0.03,True,True)
     
-
+def CheckIfPlrDead(Local_Player):
+    if Local_Player.IsDied() == True:
+            return False 
+def CheckIfenemyDead(count):
+    if count == 0:
+            return True
 def Initiate_Fight(Local_Player,Ally1,Ally2,Ally3,Ally4,Enemy1,Enemy2,Enemy3):
     while True:
-        if Local_Player.IsDied() == True:
-            return False 
-    
-        if Ally1 is not None:
-            Ally1.Status_Ending()
-            if Ally1.IsDied() == True:
-                Local_Player.Damage_Player(1)
-        if Ally2 is not None: 
-            Ally2.Status_Ending()
-            if Ally2.IsDied() == True:
-                Local_Player.Damage_Player(1)
-        if Ally3 is not None: 
-            Ally3.Status_Ending()
-            if Ally3.IsDied() == True:
-                Local_Player.Damage_Player(1)
-        if Ally4 is not None: 
-            Ally4.Status_Ending()
-            if Ally4.IsDied() == True:
-                Local_Player.Damage_Player(1)
-
-        Selected_Unit = Local_Player.SelectUnit(Ally1,Ally2,Ally3,Ally4)
-        if Selected_Unit == 1:
-            Ally1.Action(Enemy1,Enemy2,Enemy3)
-
-        if Selected_Unit == 2:
-            Ally2.Action(Enemy1,Enemy2,Enemy3)
-            if Ally1.IsDied() == True:
-                Local_Player.Take_Damage(1)
-        if Selected_Unit == 3:
-            Ally3.Action(Enemy1,Enemy2,Enemy3)
-            if Ally1.IsDied() == True:
-                Local_Player.Take_Damage(1)
-        if Selected_Unit == 4:
-            Ally4.Action(Enemy1,Enemy2,Enemy3)
-            if Ally1.IsDied() == True:
-                Local_Player.Take_Damage(1)
+        
+        CheckIfPlrDead(Local_Player)
         count = 0
-
-
-        Timed_Text("Enemies turn!",0.03,True,True)
+        
+        
         if Enemy1 is not None:
-            Enemy1.Action(Ally1,Ally2,Ally3,Ally4)
             count += 1
             if Enemy1.IsDied() == True:
                 Enemy1 = None
                 count -= 1
         if Enemy2 is not None:
-            Enemy2.Action(Ally1,Ally2,Ally3,Ally4)
             count += 1
             if Enemy2.IsDied() == True:
                 Enemy2 = None
                 count -= 1
         if Enemy3 is not None:
-            Enemy3.Action(Ally1,Ally2,Ally3,Ally4)
             count += 1
             if Enemy3.IsDied() == True:
                 Enemy3 = None
                 count -= 1
-        if count == 0:
-            return True
+
+        CheckIfenemyDead(count)
+        if Ally1 is not None:
+            Ally1.Stamina = Ally1.MaxStamina
+            Ally1.Status_Ending()
+            if Ally1.IsDied() == True:
+                Local_Player.Damage_Player(1)
+        else:
+            Local_Player.Damage_Player(1)
+        if Ally2 is not None: 
+            Ally2.Stamina = Ally2.MaxStamina
+            Ally2.Status_Ending()
+            if Ally2.IsDied() == True:
+                Local_Player.Damage_Player(1)
+        else:
+            Local_Player.Damage_Player(1)
+        if Ally3 is not None: 
+            Ally3.Stamina = Ally3.MaxStamina
+            Ally3.Status_Ending()
+            if Ally3.IsDied() == True:
+                Local_Player.Damage_Player(1)
+        else:
+            Local_Player.Damage_Player(1)
+        if Ally4 is not None: 
+            Ally4.Stamina = Ally4.MaxStamina
+            Ally4.Status_Ending()
+            if Ally4.IsDied() == True:
+                Local_Player.Damage_Player(1)
+        else:
+            Local_Player.Damage_Player(1)
+        
+        while True:
+            Selected_Unit = Local_Player.SelectUnit(Ally1,Ally2,Ally3,Ally4)
+
+            if Selected_Unit == 5:
+                break
+
+            AllyList = [Ally1, Ally2, Ally3, Ally4]
+
+            if 1 <= Selected_Unit <= 4:
+                Ally = AllyList[Selected_Unit - 1]
+
+                if Ally is None:
+                    continue
+
+                if Ally.Stamina <= 0:
+                    Timed_Text(f"{Ally.Name} is too tired, please wait...",0.03,True,True)
+                    continue
+
+                if Ally.IsDied():
+                    Local_Player.Take_Damage(1)
+                else:
+                    Ally.Action(Enemy1,Enemy2,Enemy3)
+
+                continue       
+            
+        
+
+        if Enemy1 is not None:
+            count += 1
+            if Enemy1.IsDied() == True:
+                Enemy1 = None
+                count -= 1
+        if Enemy2 is not None:
+            count += 1
+            if Enemy2.IsDied() == True:
+                Enemy2 = None
+                count -= 1
+        if Enemy3 is not None:
+            count += 1
+            if Enemy3.IsDied() == True:
+                Enemy3 = None
+                count -= 1
+
+        CheckIfPlrDead(Local_Player)
+        CheckIfenemyDead(count)
+        Timed_Text("Enemies turn!",0.03,True,True)
+        if Enemy1 is not None:
+            Enemy1.Action(Ally1,Ally2,Ally3,Ally4)
+        if Enemy2 is not None:
+            Enemy2.Action(Ally1,Ally2,Ally3,Ally4)
+        if Enemy3 is not None:
+            Enemy3.Action(Ally1,Ally2,Ally3,Ally4)
+
+        CheckIfenemyDead(count)
+        CheckIfPlrDead(Local_Player)
